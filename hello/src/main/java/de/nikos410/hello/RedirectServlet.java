9f45f2b635faa8f4cc3ca2c1fb0f8b029b6963f5
Nikos Epping <nikos.epping@protonmail.com>
2021-06-12 17:09:25 +0200

    Add redirect Servlet

---
+++ hello/src/main/java/de/nikos410/hello/RedirectServlet.java
@@ -0,0 +1,32 @@
+package de.nikos410.hello;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(name = "redirectServlet", value = "/redirect-servlet")
+public class RedirectServlet extends HttpServlet {
+
+    private static final String TARGET_PARAMETER_NAME = "target";
+    private static final String ERROR_URL = "goto.jsp?error=Ziel%20muss%20angegeben%20sein.";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        final String redirectUrl = getRedirectUrl(request);
+        response.sendRedirect(redirectUrl);
+    }
+
+    private String getRedirectUrl(HttpServletRequest request) {
+
+        return getTargetFromRequest(request)
+                .orElse(ERROR_URL);
+    }
+
+    private Optional<String> getTargetFromRequest(HttpServletRequest request) {
+
+        return Optional.ofNullable(request.getParameter(TARGET_PARAMETER_NAME));
+    }
+}
